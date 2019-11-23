7a35579264f4c80a2c5477c01e230289fbabd5f7
krishan <krishan.vadher@gmail.com>
2019-11-23 15:01:15 +0000

    update from mac

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character
+{
+    public string name;
+    public int exp = 0;
+
+    public Character()
+    {
+        name = "Not assigned";
+    }
+
+    public Character(string name)
+    {
+        this.name = name;
+    }
+
+    public void PrintStatsInfo()
+    {
+        Debug.LogFormat("Hero: {0} - {1} EXP", name, exp);
+    }
+
+}
---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    void Start()
+    {
+        int playerLives = 10;
+
+        while (playerLives > 0)
+        {
+            Debug.Log("Still alive!");
+            playerLives--;
+        }
+
+        Debug.Log("Player koooo'd");
+
+        Character hero = new Character();
+        Debug.LogFormat("Hero: {0} - {1} EXP", hero.name, hero.exp);
+
+        Character heroine = new Character("Agatha");
+        Debug.LogFormat("Hero: {0} - {1} EXP", heroine.name, heroine.exp);
+
+    }
+}
